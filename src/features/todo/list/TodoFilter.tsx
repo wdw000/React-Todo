edf086360883d9530e60f6dad41f14332df829dc
wdw000 <dowonwang01@gmail.com>
2022-07-08 02:56:43 +0900

    add list sort

--- src/features/todo/list/TodoFilter.tsx
+++ src/features/todo/list/TodoFilter.tsx
@@ -9,6 +9,7 @@ import {
   listDateSub,
   listDateToday,
   selectListDate,
+  selectOrder,
 } from "../todoSlice";
 import "./TodoFilter.css";
 
@@ -16,18 +17,7 @@ export function TodoFilter() {
   const dispatch = useDispatch();
   const [isSort, setIsSort] = useState(false);
   const date = useSelector(selectListDate).slice(2);
-
-  function handleAddDateBtn() {
-    dispatch(listDateAdd());
-  }
-
-  function handleSubDateBtn() {
-    dispatch(listDateSub());
-  }
-
-  function handleTodayDateBtn() {
-    dispatch(listDateToday());
-  }
+  const order = useSelector(selectOrder);
 
   function handleSortBtn() {
     setIsSort(!isSort);
@@ -36,82 +26,89 @@ export function TodoFilter() {
   const sort = (
     <div className="sort-list">
       <ul className="completed-filter">
-        <li className="click" onClick={() => dispatch(changeFilter("all"))}>
+        <li
+          className={"click " + (order.filter === "all" && "on")}
+          onClick={() => dispatch(changeFilter("all"))}
+        >
           전체
         </li>
         <li
-          className="click"
+          className={"click " + (order.filter === "incompleted" && "on")}
           onClick={() => dispatch(changeFilter("incompleted"))}
         >
           진행
         </li>
         <li
-          className="click"
+          className={"click " + (order.filter === "completed" && "on")}
           onClick={() => dispatch(changeFilter("completed"))}
         >
           완료
         </li>
       </ul>
-      <span>|</span>
+
       <ul className="timestamp-filter">
-        <li className="click" onClick={() => dispatch(changeLatest(true))}>
+        <li
+          className={"click " + (order.latest === true && "on")}
+          onClick={() => dispatch(changeLatest(true))}
+        >
           최신순
         </li>
-        <li className="click" onClick={() => dispatch(changeLatest(false))}>
+        <li
+          className={"click " + (order.latest === false && "on")}
+          onClick={() => dispatch(changeLatest(false))}
+        >
           오래된순
         </li>
       </ul>
-      <span>|</span>
+
       <ul className="date-filter">
-        <li className="click" onClick={() => dispatch(changeIsStart(true))}>
+        <li
+          className={"click " + (order.isStart === true && "on")}
+          onClick={() => dispatch(changeIsStart(true))}
+        >
           시작일순
         </li>
-        <li className="click" onClick={() => dispatch(changeIsStart(false))}>
+        <li
+          className={"click " + (order.isStart === false && "on")}
+          onClick={() => dispatch(changeIsStart(false))}
+        >
           마감일순
         </li>
       </ul>
-      <span>|</span>
+
       <ul className="important-filter">
-        <li className="click" onClick={() => dispatch(changeImportant())}>
+        <li
+          className={"click " + (order.important === true && "on")}
+          onClick={() => dispatch(changeImportant())}
+        >
           중요도순
         </li>
       </ul>
     </div>
   );
 
-  const item = (
-    <div className="date-control">
-      <button
-        className="date-sub click"
-        type="button"
-        onClick={() => handleSubDateBtn()}
-      >
-        &lt;
-      </button>
-      <button
-        type="button"
-        className="click"
-        onClick={() => handleTodayDateBtn()}
-      >
-        {date}
-      </button>
-      <button
-        className="date-plus click"
-        type="button"
-        onClick={() => handleAddDateBtn()}
-      >
-        &gt;
-      </button>
-    </div>
-  );
-
   return (
     <div className="TodoFilter">
-      {isSort ? sort : item}
-      <div className="sort-box click" onClick={() => handleSortBtn()}>
-        <span>
-          <span>&lt;</span> 정렬
+      <div className="date-control">
+        <div className="date">{date}</div>
+        <div className="btn-group">
+          <button className="click" onClick={() => dispatch(listDateSub())}>
+            &lt;
+          </button>
+          <button className="click" onClick={() => dispatch(listDateToday())}>
+            오늘
+          </button>
+          <button className="click" onClick={() => dispatch(listDateAdd())}>
+            &gt;
+          </button>
+        </div>
+      </div>
+
+      <div className="sort-box click">
+        <span onClick={() => handleSortBtn()}>
+          정렬<span className="extand off">&lt;</span>
         </span>
+        {isSort === true && sort}
       </div>
     </div>
   );
--- src/features/todo/list/TodoList.tsx
+++ src/features/todo/list/TodoList.tsx
@@ -2,16 +2,38 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 import TodoAddBtn from "../../../components/TodoAddBtn";
-import { selectCloseTodos, selectListTodos, selectOrder } from "../todoSlice";
+import {
+  selectCloseTodos,
+  selectListTodos,
+  selectOrder,
+  Todo,
+} from "../todoSlice";
 import { TodoFilter } from "./TodoFilter";
 import TodoItem from "../../../components/TodoItem";
 import TodoClosedWarning from "./TodoClosedWarning";
+import moment from "moment";
 
 export default function TodoList() {
   const data = useSelector(selectListTodos);
   const order = useSelector(selectOrder);
   const closeTodos = useSelector(selectCloseTodos);
-  let todos;
+  let todos: Todo[];
+
+  function compareStartDate(a: Todo, b: Todo) {
+    if (a.start_date === b.start_date) {
+      return moment(b.timestamp).diff(a.timestamp);
+    } else {
+      return moment(b.start_date).diff(a.start_date);
+    }
+  }
+
+  function compareEndDate(a: Todo, b: Todo) {
+    if (a.end_date === b.end_date) {
+      return moment(b.timestamp).diff(a.timestamp);
+    } else {
+      return moment(b.end_date).diff(a.end_date);
+    }
+  }
 
   switch (order.filter) {
     case "all":
@@ -23,6 +45,45 @@ export default function TodoList() {
     case "incompleted":
       todos = data.filter((todo) => todo.completed === false);
       break;
+  }
+
+  if (order.important) {
+    const important = todos.filter((item) => item.important === true);
+    const trivial = todos.filter((item) => item.important === false);
+
+    if (order.latest) {
+      if (order.isStart) {
+        important.sort((a, b) => compareStartDate(a, b));
+        trivial.sort((a, b) => compareStartDate(a, b));
+      } else {
+        important.sort((a, b) => compareEndDate(a, b));
+        trivial.sort((a, b) => compareEndDate(a, b));
+      }
+    } else {
+      if (order.isStart) {
+        important.sort((a, b) => compareStartDate(b, a));
+        trivial.sort((a, b) => compareStartDate(b, a));
+      } else {
+        important.sort((a, b) => compareEndDate(b, a));
+        trivial.sort((a, b) => compareEndDate(b, a));
+      }
+    }
+
+    todos = [...important, ...trivial];
+  } else {
+    if (order.latest) {
+      if (order.isStart) {
+        todos.sort((a, b) => compareStartDate(a, b));
+      } else {
+        todos.sort((a, b) => compareEndDate(a, b));
+      }
+    } else {
+      if (order.isStart) {
+        todos.sort((a, b) => compareStartDate(b, a));
+      } else {
+        todos.sort((a, b) => compareEndDate(b, a));
+      }
+    }
   }
 
   const item = todos.map((todo) => <TodoItem todo={todo} key={todo.id} />);
